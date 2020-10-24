a936d9a0342091c789df8e158cd90fb41b9bd2f0
Ivan Alexandrov <ivan@alexandrov.online>
2020-10-24 20:21:13 +0200

    Write credentials to the env file

--- example/main.js
+++ example/main.js
@@ -1,1 +1,24 @@
 import WebRTCOverXMPP from "../src/index.js";
+
+window.bot2 = new WebRTCOverXMPP({
+  jid: process.env.JID1,
+  password: process.env.PASSWORD1,
+  server: process.env.SERVER,
+  room: process.env.ROOM
+});
+
+bot2.on("message", (data, peer) => {
+  console.log(`data "${data}" from ${peer.id}`);
+});
+
+
+window.bot3 = new WebRTCOverXMPP({
+  jid: process.env.JID2,
+  password: process.env.PASSWORD2,
+  server: process.env.SERVER,
+  room: process.env.ROOM
+});
+
+bot3.on("message", (data, peer) => {
+  console.log(`data "${data}" from ${peer.id}`);
+});
